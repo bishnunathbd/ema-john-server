5f7ce7b57908663241d5200e370cb29d28a6e8a3
bishnunathbd <bishnunath774@gmail.com>
2020-12-29 19:39:08 +0600

    50-3-insert one item to the database

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
-// console.log(process.env.DB_USER); // check .env connection
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wbtxn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
 const port = 5000
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db(process.env.DB_NAME).collection("products");
-  console.log('database connected');
+  const productsCollection = client.db(process.env.DB_NAME).collection("products");
+  
+  app.post('/addProduct', (req, res) => {
+    const product = req.body;
+    console.log(product);
+    productsCollection.insertOne(product)
+    .then(result => {
+      console.log(result);
+    })
+  })
 });
 
 
