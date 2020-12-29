deea819d7f64244780c702fe34ce382529eaf757
bishnunathbd <bishnunath774@gmail.com>
2020-12-29 16:04:24 +0600

    50-2-db connection

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 // console.log(process.env.DB_USER); // check .env connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wbtxn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db(process.env.DB_NAME).collection("products");
+  console.log('database connected');
+});
+
 
 app.listen(port)
