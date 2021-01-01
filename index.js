87584377e92628b53e1243702454f78e1da8d96e
bishnunathbd <bishnunath774@gmail.com>
2021-01-01 09:38:30 +0600

    50-5-create api to find productsByKeys in mongodb

--- index.js
+++ index.js
@@ -17,27 +17,35 @@ const port = 5000
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db(process.env.DB_NAME).collection("products");
-  
+
   app.post('/addProduct', (req, res) => {
     const products = req.body;
     productsCollection.insertMany(products)
-    .then(result => {
-      console.log(result.insertedCount);
-    })
+      .then(result => {
+        console.log(result.insertedCount);
+      })
   })
 
   app.get('/products', (req, res) => {
     productsCollection.find({})
-    .toArray((err, documents) => {
-      res.send(documents);
-    })
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
   })
 
   app.get('/product/:key', (req, res) => {
-    productsCollection.find({key: req.params.key})
-    .toArray((err, documents) => {
-      res.send(documents[0]);
-    })
+    productsCollection.find({ key: req.params.key })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
+  })
+
+  app.post('/productsByKeys', (req, res) => {
+    const productKeys = req.body;
+    productsCollection.find({ key: { $in: productKeys } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
   })
 });
 
